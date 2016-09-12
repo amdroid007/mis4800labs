6bfe82e3f792e8546405b3a5b36162e5ee4eaf9a
Jit Sengupta <sengupta_jit@hotmail.com>
2016-09-12 08:52:19 -0400

    First git project

---
+++ MiniLab1/src/com/example/minilab1/MiniLab1.java
@@ -0,0 +1,26 @@
+package com.example.minilab1;
+
+public class MiniLab1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello my first Java app!");
+		sumDigits(123);
+		Student s = new Student(null);
+	}
+
+	private static void sumDigits(int number) {
+		// TODO Auto-generated method stub
+		
+		// Step 1.
+		
+		// Step 2. 
+		
+		// Step 3.
+		
+		// Return the value
+		
+		
+	}
+
+}
---
+++ MiniLab1/src/com/example/minilab1/Student.java
@@ -0,0 +1,10 @@
+package com.example.minilab1;
+
+public class Student {
+	private String name;
+	
+	public Student(String _name) {
+		name = _name;
+	}
+
+}
